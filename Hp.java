1ec5b54f5a34c66cc880507829057083ac0e39bf
mohirdi <114629894+mohirdi@users.noreply.github.com>
2024-03-02 22:31:23 +0700

    Add files via upload

---
+++ Hp.java
@@ -0,0 +1,24 @@
+public class HandPhone {
+    String jenis_hp;
+    int tahun_pembuatan;
+    
+    public void setDataHP(String jenis_hp, int tahun_pembuatan){
+        this.jenis_hp = jenis_hp;
+        this.tahun_pembuatan = tahun_pembuatan;
+    }
+    
+    public String getJenisHP(){
+        return jenis_hp;
+    }
+    
+    public int getTahunPembuatan(){
+        return tahun_pembuatan;
+    }
+    
+    public static void main(String[] args) {
+        HandPhone hp = new HandPhone();
+        hp.setDataHP("Oddo", 2022);
+        System.out.println(hp.getJenisHP());
+        System.out.println(hp.getTahunPembuatan());
+    }
+}
